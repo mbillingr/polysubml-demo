be516239f39be983075a380ab123f2d1ce68f76e
Martin Billinger-Finke <mbillingr@users.noreply.github.com>
2025-11-05 21:30:08 +0100

    add library functions

---
+++ libs/num.ml
@@ -0,0 +1,8 @@
+{
+    i2f = __int_to_float
+    ; f2i = __float_to_int
+    ; s2i = __str_to_int
+    ; s2f = __str_to_float
+    ; i2s = __int_to_str
+    ; f2s = __float_to_str
+}
--- libs/str.ml
+++ libs/str.ml
@@ -3,4 +3,11 @@
     ; split = __split
     ; escape = __escape
     ; unescape = __unescape
+
+    ; join = fun (sep, it) ->
+        let vars = {mut out = ""}
+        in loop
+            match it {} with
+                | `None _ -> `Break vars.out
+                | `Some s -> `Continue (if vars.out == "" then vars.out <- s else vars.out <- vars.out ^ sep ^ s)
 }
